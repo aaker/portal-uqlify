c00df1921ada1b8d4ff96da69d6e8cfe023cc57c
aaker <c.aaker@gmail.com>
2017-03-01 23:29:57 -0800

    first commit

---
+++ custom.js
@@ -0,0 +1,5 @@
+
+var paybill = '<li><a href="http://netsapiens.com" target="_blank" class="header-link" >Pay my Bill</a></li>';
+var opentix = '<li><a href="http://netsapiens.com" target="_blank" class="header-link" >Open a ticket</a></li>';
+$('.user-toolbar').prepend(paybill);
+$('.user-toolbar').prepend(opentix);
---
+++ custom.php
@@ -0,0 +1,2 @@
+<?php
+echo '<div id="footer"><p>Server Info: '.php_uname().'</p></div>';
